6c852d03eac1a19964f01e6b985f11c6292ce61e
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2024-07-08 14:17:23 +0000

    type-fu

--- src/types.ts
+++ src/types.ts
@@ -17,16 +17,34 @@ type Square = {
 type Shape2D = Circle | Rectangle | Square
 
 const getArea = (shape: Shape2D) => {
-	switch(shape.type) {
+	switch (shape.type) {
 		case 'circle':
-		  return Math.PI * shape.radius ** 2
+			return Math.PI * shape.radius ** 2
 		case 'rectangle':
 			return shape.side1 * shape.side2
 		case 'square':
 			return shape.side ** 2
 		default:
 			// exhaustiveness check
-			const x: never = shape	
+			const x: never = shape
 			throw new Error('this will not compile anyway')
+	}
+}
+
+const isCircle = (shape: Shape2D) => {
+	return shape.type === 'circle'
+}
+
+const isSquare = (shape: Shape2D) => {
+	return 'side' in shape
+}
+
+const getCircumference = (shape: Shape2D) => {
+	if (isCircle(shape)) {
+		return 2 * Math.PI * shape.radius
+	} else if (isSquare(shape)) {
+		return 4 * shape.side
+	} else {
+		return 2 * (shape.side1 + shape.side2)
 	}
 }
