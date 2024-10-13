2213ec2b007564a05defe72ba0f2ee2d27fb47cf
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2024-10-13 19:46:56 +0000

    composition

---
+++ src/features/todo/todo-web-request.ts
@@ -0,0 +1,17 @@
+import { composeWith } from 'ramda'
+
+type CreateTodoWebRequest = {
+    headers: string[],
+    body: FormData,
+}
+
+type CreateTodoWebRequestWithParsedBody = CreateTodoWebRequest & {
+    parsedBody: unknown,
+
+}
+
+const parseCreateTodoWebRequest = (body: unknown) => {
+}
+
+export const handleCreateTodoWebRequest = composeWith(() => 1,
+    [() => 1])
