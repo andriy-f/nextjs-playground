0967a04c6f7f0b92e374ef85f2a6e6e5f658b8e9
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2024-11-25 21:14:12 +0000

    fix comment

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,24 +4,27 @@
 
 // import { type NextRequest, NextResponse } from "next/server";
 
-export { auth as middleware } from "@/features/auth/auth";
+import { auth } from "@/features/auth/auth";
 
-// export const middleware = async (request: NextRequest) => {
+// export const middleware = auth(async (request: NextRequest) => {
 //     if (request.nextUrl.pathname.startsWith('/dashboard')) {
 //         console.log('middleware.ts: redirecting dashboard to /auth/signin')
 //         return NextResponse.rewrite(new URL('/auth/signin', request.url))
 //     }
-// }
+// })
+
+export const middleware = auth
 
 export const config = {
     // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
     /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
-     * and .png files (images)
+     * Match all request paths except 
+     *   a) for the ones starting with:
+     *   - api (API routes)
+     *   - _next/static (static files)
+     *   - _next/image (image optimization files)
+     *   - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     *   b) and .png files (images)
      */
     matcher: ['/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.*\\.png$).*)'],
 };
