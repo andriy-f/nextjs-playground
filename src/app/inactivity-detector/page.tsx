542ddbc9cae95e75454a5509ded2d186847d939b
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2024-11-27 22:38:36 +0000

    code style

--- src/app/inactivity-detector/page.tsx
+++ src/app/inactivity-detector/page.tsx
@@ -1,10 +1,10 @@
 import InactivityDetector from "@/features/inactivity-detector/InactivityDetector"
 
 const page: React.FC = () => {
-    return (<>
-        <h1>Inactivity page</h1>
-        <InactivityDetector />
-    </>)
+	return (<>
+		<h1>Inactivity page</h1>
+		<InactivityDetector />
+	</>)
 }
 
 export default page
--- src/app/person/page.tsx
+++ src/app/person/page.tsx
@@ -3,12 +3,12 @@ import React from 'react'
 import PersonForm from '@/features/person/PersonForm'
 
 const Page: React.FC = () => {
-    return (
-        <div className='max-w-lg ml-auto mr-auto pt-12'>
-            <h1 className='text-3xl text-center'>Formik samples</h1>
-            <PersonForm />
-        </div>
-    )
+	return (
+		<div className='max-w-lg ml-auto mr-auto pt-12'>
+			<h1 className='text-3xl text-center'>Formik samples</h1>
+			<PersonForm />
+		</div>
+	)
 }
 
 export default Page
