8aa08828e71a44e613ea66e020a894447fb9c826
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2024-11-28 15:16:18 +0000

    sign in form via react-hook-form

--- src/features/auth/signInForm.tsx
+++ src/features/auth/signInForm.tsx
@@ -1,67 +1,75 @@
 'use client';
 
-import { lusitana } from '@/features/shared/ui/fonts';
+import {
+	useState
+} from 'react';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 import {
 	AtSymbolIcon,
 	KeyIcon,
 	ExclamationCircleIcon,
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
+import { lusitana } from '@/features/shared/ui/fonts';
 import { Button } from '@/features/shared/ui/button';
-import { useActionState } from 'react';
 import { signInAction } from './serverActions'
+import { signInSchema } from '@/features/auth/validation';
+import PInput from '@/shared/ui/PInputWithLabel';
+import ErrorUnderInput from '@/shared/ui/ErrorUnderInput';
 
 export default function SignInForm() {
-	const [errorMessage, formAction, isPending] = useActionState(
-		signInAction,
-		undefined,
-	);
+	const [isPending, setIsPending] = useState(false);
+	const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+	const { register, handleSubmit, formState } = useForm({
+		resolver: zodResolver(signInSchema),
+	});
 
 	return (
-		<form action={formAction} className="space-y-3">
+		<form onSubmit={handleSubmit((fieldValues) => {
+			const formData = new FormData();
+			formData.append('email', fieldValues.email);
+			formData.append('password', fieldValues.password);
+
+			// Submit data
+			setIsPending(true);
+			signInAction(undefined, formData)
+				.then((signInResult) => {
+					setErrorMessage(signInResult ?? null);
+				})
+				.catch((error) => {
+					setErrorMessage(error.message ?? 'Something else went wrong.');
+				})
+				.finally(() => {
+					setIsPending(false);
+				})
+		})} className="space-y-3">
 			<div className="flex-1 rounded-lg px-6 pb-4 pt-8">
 				<h1 className={`${lusitana.className} mb-3 text-2xl`}>
 					Please log in to continue.
 				</h1>
-				<div className="w-full">
+				<div className="w-full space-y-1">
 					<div>
-						<label
-							className="mb-3 mt-5 block text-xs font-medium"
-							htmlFor="email"
-						>
-							Email
-						</label>
-						<div className="relative">
-							<input
-								className="peer block w-full rounded-md border py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-								id="email"
-								type="email"
-								name="email"
-								placeholder="Enter your email address"
-								required
-							/>
-							<AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2" />
-						</div>
+						<PInput
+							label="Email"
+							icon={<AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2" />}
+							{...register('email')}
+						/>
+						<ErrorUnderInput>
+							{formState.errors.email?.message?.toString()}
+						</ErrorUnderInput>
 					</div>
-					<div className="mt-4">
-						<label
-							className="mb-3 mt-5 block text-xs font-medium "
-							htmlFor="password"
-						>
-							Password
-						</label>
-						<div className="relative">
-							<input
-								className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-								id="password"
-								type="password"
-								name="password"
-								placeholder="Enter password"
-								required
-								minLength={6}
-							/>
-							<KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2" />
-						</div>
+					<div>
+						<PInput
+							label="Password"
+							type="password"
+							icon={<KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2" />}
+							{...register('password')}
+						/>
+						<ErrorUnderInput>
+							{formState.errors.password?.message?.toString()}
+						</ErrorUnderInput>
 					</div>
 				</div>
 				<Button className="mt-4 w-full" aria-disabled={isPending}>
