d7228eef7f3be3142405a377c497aaddb7ccea66
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2024-11-28 15:02:46 +0000

    better signout page

--- src/app/auth/signout/page.tsx
+++ src/app/auth/signout/page.tsx
@@ -1,16 +1,17 @@
 'use client'
 
-import {signOutAction} from "@/features/auth/serverActions";
+import { signOutAction } from "@/features/auth/serverActions";
+import { Button } from "@/features/shared/ui/button";
 
 const SignOut = () => {
 	return (
-		<div>
-			Are you sure to sign out?
-			<div>
-				<button onClick={signOutAction}>Yes</button>
-				<button onClick={() => {
+		<div className="container mx-auto">
+			<h2 className="font-bold text-xl">Are you sure to sign out?</h2>
+			<div className="flex space-x-1">
+				<Button onClick={signOutAction}>Yes</Button>
+				<Button onClick={() => {
 					window.history.back()
-				}}>Back</button>
+				}}>Back</Button>
 			</div>
 		</div>
 	)
