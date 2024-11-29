18f6c00a44a887c4c283e33d4c38166f92d94de8
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2024-11-29 12:00:54 +0000

    improve mobile menu ux

--- src/features/header/SiteHeader.tsx
+++ src/features/header/SiteHeader.tsx
@@ -73,13 +73,11 @@ export default async function Navbar() {
 					{headerNavigation.map((item) => (
 						<DisclosureButton
 							key={item.name}
-							as="a"
+							as={NavLink}
 							href={item.href}
-							aria-current={false ? 'page' : undefined}
-							className={clsx(
-								false ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-								'block rounded-md px-3 py-2 text-base font-medium',
-							)}
+							className='block rounded-md px-3 py-2 text-base font-medium'
+							activeClassName='bg-gray-900 text-white'
+							nonActiveClassName='text-gray-300 hover:bg-gray-700 hover:text-white'
 						>
 							{item.name}
 						</DisclosureButton>
