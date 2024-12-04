2a52fe85cb6734e122ccd3a0a624a87b5ddf774b
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2024-12-04 19:48:24 +0000

    some info on profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Profile from '@/features/profile/Profile';
 
 const ProfilePage: React.FC = () => {
 	return (
 		<div>
-			<h1>Profile Page</h1>
-			<p>Welcome to the profile page!</p>
+			<section className='container mx-auto'>
+				<h1 className='text-3xl'>Profile Page</h1>
+				<Profile />
+			</section>
 		</div>
 	);
 };
---
+++ src/features/profile/Profile.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { requireAuthentication } from '@/features/shared/dal';
+
+const PLabel: React.FC<React.PropsWithChildren> = (props) => (<label
+	className='block'
+>{props.children}</label>)
+
+const Profile: React.FC = async () => {
+	const userData = await requireAuthentication();
+	return (
+		<div className='flex flex-col gap-4'>
+			<PLabel>
+				id
+				<input
+					className='block'
+					type="text" defaultValue={userData.id} readOnly />
+			</PLabel>
+			<PLabel>
+				email
+				<input
+					className='block'
+					type="text"
+					defaultValue={userData.email ?? undefined} readOnly />
+			</PLabel>
+			<PLabel>
+				name
+				<input
+					className='block'
+					type="text"
+					defaultValue={userData.name ?? undefined} readOnly />
+			</PLabel>
+		</div>
+	);
+};
+
+export default Profile;
