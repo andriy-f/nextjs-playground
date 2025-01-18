47522af44eb0be5f9c6068d099eb5ed2e351465a
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2025-01-18 22:39:35 +0000

    auth requirements and persission requirements in sitePath declaration

--- src/features/shared/sitePaths.ts
+++ src/features/shared/sitePaths.ts
@@ -1,35 +1,51 @@
 export type SitePath = {
 	name: string;
 	href: string;
+	requireAuthentication: boolean;
+	requirePermissions: string[];
 }
 
 export const sitePaths = {
 	signIn: {
 		name: 'Sign In',
 		href: '/auth/signin',
+		requireAuthentication: false,
+		requirePermissions: []
 	},
 	signOut: {
 		name: 'Sign Out',
 		href: '/auth/signout',
+		requireAuthentication: true,
+		requirePermissions: []
 	},
 	home: {
 		name: 'Home',
 		href: '/',
+		requireAuthentication: false,
+		requirePermissions: []
 	},
 	dashboard: {
 		name: 'Dashboard',
 		href: '/dashboard',
+		requireAuthentication: true,
+		requirePermissions: []
 	},
 	profile: {
 		name: 'Profile',
 		href: '/profile',
+		requireAuthentication: true,
+		requirePermissions: []
 	},
 	todo: {
 		name: 'Todo',
 		href: '/todo-list',
+		requireAuthentication: true,
+		requirePermissions: ['todo:view']
 	},
 	inactivityDetector: {
 		name: 'Inactivity Detector',
 		href: '/inactivity-detector',
+		requireAuthentication: false,
+		requirePermissions: []
 	},
 } satisfies { [key: string]: SitePath }
