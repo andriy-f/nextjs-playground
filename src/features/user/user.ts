cdbf3405731516ac59188ed26dda491180a2e971
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2025-01-23 19:10:57 +0000

    optimize

--- src/features/user/user.ts
+++ src/features/user/user.ts
@@ -1,5 +1,6 @@
 import db from '@/db';
 import bcrypt from 'bcrypt'
+import { UserCredentials } from '../auth/types';
 
 type VerifyUserCredentialsResult = {
 	valid: true,
@@ -14,7 +15,7 @@ type VerifyUserCredentialsResult = {
 	user: null
 }
 
-export const verifyUserCredentials = async ({ email, password }: { email: string, password: string }): Promise<VerifyUserCredentialsResult> => {
+export const verifyUserCredentials = async ({ email, password }: UserCredentials): Promise<VerifyUserCredentialsResult> => {
 	const user = await db.user.findUnique({
 		where: {
 			email,
@@ -36,16 +37,28 @@ export const verifyUserCredentials = async ({ email, password }: { email: string
 		}
 	})
 
-	if (!user) return { valid: false, user: null }
-	else {
-		const isCorrectPassword = await bcrypt.compare(password, user.passwordHash)
-		return isCorrectPassword ? {
-			valid: true, user: {
-				id: user.id,
-				name: user.name,
-				email: user.email,
-				permissions: user?.roles.flatMap((role) => role.permissions.map((permission) => permission.code)) ?? [],
-			}
-		} : { valid: false, user: null }
-	}
+	return user !== null
+		? (
+			await bcrypt.compare(password, user.passwordHash)
+				// when password matches
+				? {
+					valid: true,
+					user: {
+						id: user.id,
+						name: user.name,
+						email: user.email,
+						permissions: user?.roles.flatMap((role) => role.permissions.map((permission) => permission.code)) ?? [],
+					}
+				}
+				// when password does not match
+				: {
+					valid: false,
+					user: null
+				}
+		)
+		// when user not found by email
+		: {
+			valid: false,
+			user: null
+		}
 }
