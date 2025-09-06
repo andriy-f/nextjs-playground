9c9d81d6851cdb65bc6263f15f3ca466cc21969d
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2025-09-06 16:13:31 +0000

    update zod to v4

--- src/features/auth/validation.ts
+++ src/features/auth/validation.ts
@@ -1,17 +1,26 @@
 import { z } from "zod"
 
 export const userCredentialsSchema = z.object({
-	email: z.string({ required_error: "Email is required" })
-		.min(1, "Email is required")
-		.email("Invalid email"),
-	password: z.string({ required_error: "Password is required" })
+	email: z.email({
+		error: (issue) => issue.input === undefined
+			? "This field is required"
+			: "Not an email"
+	})
+		.min(1, "Email is required"),
+	password: z.string(
+		{
+			error: (issue) => issue.input === undefined
+				? "This field is required"
+				: "Not a string"
+		}
+	)
 		.min(1, "Password is required")
 		.max(32, "Password must be less than 32 characters"),
 })
 
 export const sessionUserSchema = z.object({
 	id: z.string(),
-	email: z.string(),
+	email: z.email(),
 	name: z.string().nullable(),
 	permissions: z.array(z.string()),
 })
